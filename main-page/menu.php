ee691b21c271a4cc63902595798b35d7279cf415
Bartosz Piętka <126457799+bartoszpietka@users.noreply.github.com>
2024-04-03 10:00:15 +0200

    a

--- main-page/menu.php
+++ main-page/menu.php
@@ -10,9 +10,9 @@ session_start()
     <style>
         #header{
             display: grid;
-            grid-template-columns: 38% 47% 7% 8%;
+            grid-template-columns: 8% 7% 70% 7% 8%;
             grid-template-rows: 140px;
-            grid-template-areas: "homepage searchbar userprofile shoppingcart";
+            grid-template-areas: "homepage null searchbar userprofile shoppingcart";
             align-items: center;
         }
     </style>
@@ -22,8 +22,10 @@ session_start()
         <a href="http://localhost/sklep/main-page/" style="color: #00000000; grid-area: homepage;">
             <img src="../images/logo.png" alt="logo" style="margin: 20px; height: 100px;">
         </a>
+        
+        <div style="grid-area: null;"></div>
 
-        <form action="../search/" id="searchform" style="grid-area: searchbar;">
+        <form action="../search/" id="searchform" style="grid-area: searchbar; display: flex; justify-content: center;">
             <input type="text" id="searchinput" name="searchedphrase">
             <button id="searchbutton" type="submit">
                 <img src="../images/search.png" alt="wyszukaj" style="height: 19px;">
